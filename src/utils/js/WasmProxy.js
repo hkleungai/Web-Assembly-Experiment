e3625fe78b2e0cc96f43c1803b6691cf79442e72
Jimmy Leung <mingfamily@hotmail.com.hk>
2022-12-27 20:33:16 +0800

    test: add test for WasmError.js and WasmProxy.js

--- src/utils/js/WasmProxy.js
+++ src/utils/js/WasmProxy.js
@@ -7,16 +7,20 @@ export default class WasmProxy {
     function make_validator(...path) {
       return {
         get(self, key) {
-          const has_key = self.hasOwnProperty(key);
-          const is_object = typeof self[key] === 'object' && self[key] !== null;
           /* If self[key] points to primitive value, return it directly. */
-          if (has_key && !is_object)
+          const is_object = typeof self[key] === 'object' && self[key] !== null;
+          if (self[key] && !is_object)
             return self[key];
-          /* Else construct new "_self" and "_validator" and wrap them with Proxy */
+          
+            /* Else construct new "_self" and "_validator" and wrap them with Proxy */
           const _self = is_object ? self[key] : function () { throw new WasmError([...path, key].join('.')) };
           const _validator = make_validator(...path, key);
-          self[key] = new Proxy(_self, _validator);
-          return self[key];
+          const proxy = new Proxy(_self, _validator);
+          
+          /* Avoid re-creating function() over and over upon accessing proxy's properties */
+          Reflect.set(self, key, proxy);
+
+          return proxy;
         },
       }
     }
