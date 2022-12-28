55da7192cf9e0900f3819ddaa44749741d719857
hkleungai <hkleungai@users.noreply.github.com>
2022-12-28 05:06:17 +0000

    deploy: 97ac7eebe57a30d733b4675225b5b671dfc3742b

--- array_demo/main.cpp
+++ array_demo/main.cpp
@@ -2,24 +2,22 @@
 #include "../utils/c/Array.h"
 #include "Internal.h"
 
-EXTERN {
-  Array<Array<char>>* get_strings() {
-    return &Internal::strings;
-  };
+EXTERN Array<Array<char>>* get_strings() {
+  return &Internal::strings;
+};
 
-  Array<char> *get_string_at(Array<Array<char>> *strings, int index) {
-    return strings->at(index);
-  }
-
-  Array<Array<int>> *get_ints() {
-    return &Internal::ints;
-  };
-
-  Array<int> *get_int_at(Array<Array<int>> *ints, int index) {
-    return ints->at(index);
-  }
-
-  int get_array_attr_count() {
-    return BaseArray::ATTR_COUNT;
-  };
+EXTERN Array<char> *get_string_at(Array<Array<char>> *strings, int index) {
+  return strings->at(index);
 }
+
+EXTERN Array<Array<int>> *get_ints() {
+  return &Internal::ints;
+};
+
+EXTERN Array<int> *get_int_at(Array<Array<int>> *ints, int index) {
+  return ints->at(index);
+}
+
+EXTERN int get_array_attr_count() {
+  return BaseArray::ATTR_COUNT;
+};
--- utils/c/extern.h
+++ utils/c/extern.h
@@ -1,10 +1,24 @@
 #ifndef __USER_DEFINED__EXTERN_HEADER
 #define __USER_DEFINED__EXTERN_HEADER
 
+#define __VISIBLE__   __attribute__((visibility("default")))
+#define __INVISIBLE__ __attribute__((visibility("hidden")))
+
 #ifdef __cplusplus
-#define EXTERN extern "C"
+  #define __EXTERN__ extern "C"
 #else
-#define EXTERN
+  #define __EXTERN__
 #endif
+
+/* A "decorator" for exporting function to wasm */
+#define EXTERN __EXTERN__ __VISIBLE__
+
+/**
+ * [RISKY]
+ * Unexport "_start" symbol from WASI SDK
+ * FIXME: Find a way to somehow hack the below warning.
+ *        wasm-ld: warning: function signature mismatch: _start
+ */
+__EXTERN__ __INVISIBLE__ void _start(int) {};
 
 #endif
