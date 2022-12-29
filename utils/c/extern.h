dd7b00d6cefbea15862063b6b6b637411d2325e9
hkleungai <hkleungai@users.noreply.github.com>
2022-12-29 18:10:43 +0000

    deploy: 31b15a8d6621c1a0e0f7a6a0665eb37616488a64

--- utils/c/extern.h
+++ utils/c/extern.h
@@ -2,7 +2,6 @@
 #define __USER_DEFINED__EXTERN_HEADER
 
 #define __VISIBLE__   __attribute__((visibility("default")))
-#define __INVISIBLE__ __attribute__((visibility("hidden")))
 
 #ifdef __cplusplus
   #define __EXTERN__ extern "C"
@@ -12,13 +11,5 @@
 
 /* A "decorator" for exporting function to wasm */
 #define EXTERN __EXTERN__ __VISIBLE__
-
-/**
- * [RISKY]
- * Unexport "_start" symbol from WASI SDK
- * FIXME: Find a way to somehow hack the below warning.
- *        wasm-ld: warning: function signature mismatch: _start
- */
-__EXTERN__ __INVISIBLE__ void _start(int) {};
 
 #endif
