027bd6d8426486c3fc287d5882ca484b9f28f6a0
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2024-02-03 00:49:40 +0800

    fix the damn llvm script

--- script/install.mk
+++ script/install.mk
@@ -30,9 +30,13 @@ install--llvm: install--binaryen
 
 install--llvm-and-wasi-sdk: install--llvm
 	$(eval LLVM_VERSION := $(shell brew list llvm --versions | sed -e "s/llvm //g"))
-	$(eval CLANG_VERSION := $(shell echo "$(LLVM_VERSION)" | sed -r "s/_[0-9]+//g"))
-	$(eval BREW_PKG_DIR := $(shell brew --prefiX llvm | sed "s/\/llvm//g"))
-	
+
+	$(eval FULL_CLANG_VERSION := $(shell echo "$(LLVM_VERSION)" | sed -r "s/_[0-9]+//g"))
+	$(eval MINOR_CLANG_VERSION := $(shell echo "$(FULL_CLANG_VERSION)" | sed -r "s/.[0-9]+$$//g"))
+	$(eval MAJOR_CLANG_VERSION := $(shell echo "$(MINOR_CLANG_VERSION)" | sed -r "s/.[0-9]+$$//g"))
+
+	$(eval BREW_PKG_DIR := $(shell brew --prefix llvm | sed "s/\/llvm//g"))
+
 	@echo "Installing WASI SDK..."
 	\
 	cd $(BREW_PKG_DIR) \
@@ -41,13 +45,17 @@ install--llvm-and-wasi-sdk: install--llvm
 
 	@echo "Installing WASI Magic Tar..."
 	\
-  	cd $(BREW_PKG_DIR) \
+	cd $(BREW_PKG_DIR) \
 	&& $(SUDO_OR_NORMAL) wget $(WASI_SDK_RELEASE_DOMAIN)/$(WASK_SDK_MAGIC_TAR) \
-	&& $(SUDO_OR_NORMAL) tar xvf $(WASK_SDK_MAGIC_TAR) -C ./llvm/lib/clang/$(CLANG_VERSION)
+	&& ( \
+		$(SUDO_OR_NORMAL) tar xvf $(WASK_SDK_MAGIC_TAR) -C ./llvm/lib/clang/$(FULL_CLANG_VERSION) \
+		|| $(SUDO_OR_NORMAL) tar xvf $(WASK_SDK_MAGIC_TAR) -C ./llvm/lib/clang/$(MINOR_CLANG_VERSION) \
+		|| $(SUDO_OR_NORMAL) tar xvf $(WASK_SDK_MAGIC_TAR) -C ./llvm/lib/clang/$(MAJOR_CLANG_VERSION) \
+	)
 
 	@echo "Cleaning up..."
 	\
 	cd $(BREW_PKG_DIR) \
 	&& $(SUDO_OR_NORMAL) rm $(WASK_SDK_SYSROOT_TAR) $(WASK_SDK_MAGIC_TAR)*;
-  
+
 	@echo "Installation on WASI SDK DONE"
