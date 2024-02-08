306445daf0afaae01705ef37de6d6d1d22fbb50b
hkleungai <hkleungai@users.noreply.github.com>
2024-02-08 10:02:44 +0000

    deploy: d361a5d8afedb2ba09b9a82d3c46412c581735d8

--- utils/js/WasmProxy.js
+++ utils/js/WasmProxy.js
@@ -6,12 +6,14 @@ export default class WasmProxy {
   constructor(target = {}, paths = []) {
     return new Proxy(target, {
       get(self, key) {
-        if (self[key] && typeof self[key] !== 'object') {
-          return self[key];
-        }
         const new_paths = [...paths, key];
-        const new_target = self[key] || ((...args) => { throw new WasmError(new_paths, args) });
-        Reflect.set(self, key, new WasmProxy(new_target, new_paths));
+        if (self[key] === undefined || self[key] === null) {
+          const new_target = (...args) => { throw new WasmError(new_paths, args) };
+          Reflect.set(self, key, new WasmProxy(new_target, new_paths));
+        }
+        if (typeof self[key] === 'object') {
+          Reflect.set(self, key, new WasmProxy(self[key], new_paths));
+        }
         return self[key];
       },
     });
